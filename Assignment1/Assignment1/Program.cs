dff5e9f963aed74a2d3b6b1a6ee0a36c23ebb169
shibu-barmon <shibubarmon34@gmail.com>
2019-09-13 11:31:50 +0600

    Assignment1

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Write("Box");
+            Console.Write("Table");
+            Console.Write("chair");
+            Console.WriteLine();
+            Console.WriteLine("desk");
+        }
+    }
+}
+
+
